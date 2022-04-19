aedcca3cb6d7673357fe117f02577b00e86827f5
nicollasisaac <nicollasisaac012@gmail.com>
2022-04-19 00:39:02 -0300

    animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+const title = document.querySelector('.contain-text h1');
+
+function typeWrite(element){
+    let x = 75;
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = '';
+    textArray.forEach((letter, i) => {
+        setTimeout(()=> element.innerHTML += letter , x += 105)
+    });
+}
+
+typeWrite(title);
+
